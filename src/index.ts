f5b88508b5faaf63dab4f71f22bedc173af6d070
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-07-08 16:26:30 +0300

    add lint command and fix lint issues, typedoc comments, command and deps.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-export function add(a, b){
+/**
+ * Adds two numbers together.
+ *
+ * @remarks
+ * This method is just a simple example.
+ *
+ * @param a - The first number to add
+ * @param b - The second number to add
+ * @returns The sum of `a` and `b`
+ *
+ * @beta
+ */
+export function add(a: number, b: number) {
   return a + b;
 }
